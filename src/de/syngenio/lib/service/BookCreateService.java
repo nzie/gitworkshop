508075d5d4841c83a6ccf46d7efc9c23b94d44f4
greuelm <marcus.greuel@external.telekom.de>
2012-07-18 14:47:04 +0200

    first impl of BookCreateService

--- src/de/syngenio/lib/service/BookCreateService.java
+++ src/de/syngenio/lib/service/BookCreateService.java
@@ -1,10 +1,21 @@
 package de.syngenio.lib.service;
 
+import de.syngenio.lib.dao.BookRegistry;
+import de.syngenio.lib.domainobject.Book;
+import de.syngenio.lib.io.CharacterReader;
+
 public class BookCreateService implements IMenuChoiceService{
 
 	@Override
 	public void optionSelected() {
-		System.out.println("mybranch: throw new UnsupportedOperationException();");
+		String name = CharacterReader.readString("Name:");
+		String isbn = CharacterReader.readString("ISBN:");
+		Book book = new Book();
+		book.setName(name);
+		book.setIsbn(isbn);
+
+		BookRegistry.registerBook(book);
+		System.out.println( "New ID:"+book.getId());
 	}
 
 }
