436aed5443eb049dfe5b8a7c434b572e19d6b191
twei <thomas.weingardt@syngenio.de>
2012-07-18 14:41:10 +0200

    show all not-rented books

--- src/de/syngenio/lib/service/BookRentService.java
+++ src/de/syngenio/lib/service/BookRentService.java
@@ -1,10 +1,26 @@
 package de.syngenio.lib.service;
 
+import java.util.Collection;
+
+import de.syngenio.lib.dao.BookDao;
+import de.syngenio.lib.domainobject.Book;
+import de.syngenio.lib.io.CharacterReader;
+
 public class BookRentService implements IMenuChoiceService{
+	
+	private BookDao dao;
 
 	@Override
 	public void optionSelected() {
-		System.out.println("This operation is not supported yet");
+		this.dao = new BookDao();
+		Collection<Book> booksFound = dao.findAllBooks();
+		System.out.println("Folgende Bücher sind registriert:");
+		for (Book book : booksFound) {
+			if(!book.isRented()) {
+				System.out.println("" + book);	
+			}
+		}
+		CharacterReader.readString("Bitte geben Sie das auszuleihende Buch an: ");
 	}
 
 }
