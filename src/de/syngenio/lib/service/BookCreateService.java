9af45fe096043570cf7de605a5a340a9166c848c
Soeren <soeren.meier@gmail.com>
2012-07-18 14:33:09 +0200

    another merge

--- src/de/syngenio/lib/service/BookCreateService.java
+++ src/de/syngenio/lib/service/BookCreateService.java
@@ -1,9 +1,15 @@
 package de.syngenio.lib.service;
 
+import de.syngenio.lib.dao.BookDao;
+
 public class BookCreateService implements IMenuChoiceService{
 
+	BookDao bookDao;
+	
 	@Override
 	public void optionSelected() {
+		bookDao.deleteBook();
+		System.out.println("hello");
 		System.out.println("throw new UnsupportedOperationException ");
 	}
 
