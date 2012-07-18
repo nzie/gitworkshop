a0db69923ebf69b3be941cc87febc3c2d638e611
karalus <andre@karalus-it.de>
2012-07-18 15:01:30 +0200

    unser Beitrag

--- src/de/syngenio/lib/service/BookDeletionService.java
+++ src/de/syngenio/lib/service/BookDeletionService.java
@@ -1,10 +1,29 @@
 package de.syngenio.lib.service;
 
+import java.util.Collection;
+
+import de.syngenio.lib.dao.BookDao;
+import de.syngenio.lib.domainobject.Book;
+import de.syngenio.lib.io.CharacterReader;
+
 public class BookDeletionService implements IMenuChoiceService{
+
+	private BookDao bookDao;
 
 	@Override
 	public void optionSelected() {
-	System.err.println("throw new UnsupportedOperationException();");
+		CharacterReader.readString("Bitte geben Sie die Nummer des Buches zum löschen");
+		Integer index = CharacterReader.readIntegerFromConsole();
+		Book book = bookDao.findBookById(index);
+		bookDao.deleteBook(book);
+	}
+
+	public BookDao getBookDao() {
+		return bookDao;
+	}
+
+	public void setBookDao(BookDao bookDao) {
+		this.bookDao = bookDao;
 	}
 
 }
