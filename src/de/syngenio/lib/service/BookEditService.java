46ae817c422f5698e02388d2ad8f7360f134950c
Soeren <soeren.meier@gmail.com>
2012-07-18 14:38:06 +0200

    merge

--- src/de/syngenio/lib/service/BookEditService.java
+++ src/de/syngenio/lib/service/BookEditService.java
@@ -1,9 +1,14 @@
 package de.syngenio.lib.service;
 
+import de.syngenio.lib.dao.BookDao;
+
 public class BookEditService implements IMenuChoiceService{
 
+	BookDao bookDao;
+	
 	@Override
 	public void optionSelected() {
+		bookDao.findAllBooks();
 		System.out.println("not implemented yet");
 	}
 
