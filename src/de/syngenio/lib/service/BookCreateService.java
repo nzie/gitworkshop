dc9226af2cc39c2ba87afca4da72c977e4a66567
Nico Ziegenhals <nziegenhals@gmail.com>
2012-07-18 14:40:23 +0200

    added first version

--- src/de/syngenio/lib/service/BookCreateService.java
+++ src/de/syngenio/lib/service/BookCreateService.java
@@ -1,16 +1,19 @@
 package de.syngenio.lib.service;
 
 import de.syngenio.lib.dao.BookDao;
+import de.syngenio.lib.dao.BookRegistry;
+import de.syngenio.lib.domainobject.Book;
+import de.syngenio.lib.io.CharacterReader;
 
 public class BookCreateService implements IMenuChoiceService{
 
-	BookDao bookDao;
 	
 	@Override
 	public void optionSelected() {
-		bookDao.deleteBook();
-		System.out.println("hello");
-		System.out.println("throw new UnsupportedOperationException ");
+		String bookanme = CharacterReader.readString("Please insert bookname");
+		Book book = new Book();
+		book.setName(bookanme);
+		BookRegistry.registerBook(book);
 	}
 
 }
